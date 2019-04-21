7086b8a70d69e447cfb81807ba3bd976d76c6d6a
Shruthi-Rampure <shruthi.rampure11@gmail.com>
2019-04-21 12:30:19 +0000

    added code for the new feature

--- voiceMain.java
+++ voiceMain.java
@@ -2,7 +2,8 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
      // prints "Welcome to git, in the terminal.
-System.out.println("Welcome to Git"); 
+System.out.println("Welcome to Git");
+System.out.println("branch created with branch name feature-1"); 
     }
 }
 
