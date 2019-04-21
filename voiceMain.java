d22128fc34c295250745d9eb4a7dc2d6c446dbb5
Shruthi Rampure <shruthi.rampure11@gmail.com>
2019-04-21 07:09:34 +0000

    Added comment lines at the top of the code

--- voiceMain.java
+++ voiceMain.java
@@ -7,8 +7,13 @@ public class HelloWorld {
 
 
 
-
-
+//modifyingg
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
+}
 
 
 
