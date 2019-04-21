3da77f7be6e9d570372c272b935b533b52ac709c
Shruthi Rampure <shruthi.rampure11@gmail.com>
2019-04-21 07:58:59 +0000

    Added comment line for new statement

--- voiceMain.java
+++ voiceMain.java
@@ -1,6 +1,8 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+     // prints "Welcome to git, in the terminal.
+System.out.println("Welcome to Git"); 
     }
 }
 
