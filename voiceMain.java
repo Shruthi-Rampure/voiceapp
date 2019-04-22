0e6cb23c71583103222c29f191329ce4eef9b2fe
Shruthi-Rampure <49831612+Shruthi-Rampure@users.noreply.github.com>
2019-04-22 20:21:14 +0530

    Update voiceMain.java

--- voiceMain.java
+++ voiceMain.java
@@ -3,7 +3,7 @@ class HelloWorld {
         System.out.println("Hello, World!");
      // prints "Welcome to git, in the terminal.
 System.out.println("Welcome to Git");
-System.out.println("branch created with branch name feature-1"); 
+System.out.println("branch created with branch name feature-2"); 
     }
 }
 
